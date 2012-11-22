2b51dc4479137c5b86f3ab40a325c850d1d04f95
Kate Harrison <harriska@eecs.berkeley.edu>
2012-11-22 00:58:18 -0800

    Updated documentation for Helpers/data_exists.m.

--- Helpers/data_exists.m
+++ Helpers/data_exists.m
@@ -1,5 +1,7 @@
 function [ result ] = data_exists(label)
-%   [ result ] = exists(label)
+%   [ result ] = data_exists(label)
+%
+%   Returns 'true' if the data exists and 'false' otherwise.
 
 filename = generate_filename(label);
 result = (exist([filename '.mat'], 'file') == 2);
