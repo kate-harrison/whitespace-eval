b5ab4f464cce3385fed84e5d625473522117f979
Kate Harrison <harriska@eecs.berkeley.edu>
2013-06-10 21:37:33 -0700

    Adding a helper make_data_for_all_map_sizes() which creates the data for a given label for all valid map sizes (as given in get_simulation_value.m).

---
+++ Helpers/make_data_for_all_map_sizes.m
@@ -0,0 +1,20 @@
+function [] = make_data_for_all_map_sizes(label)
+
+if ~isfield(label, 'map_size')
+    error('Map size does not appear to matter for this label.');
+end
+
+map_sizes = get_simulation_value('valid_map_sizes');
+
+for ms = 1:length(map_sizes)
+    label.map_size = map_sizes{ms};
+    try
+        validate_label(label);
+        make_data(label);
+    catch err
+        disp(err);
+    end
+end
+
+
+end
