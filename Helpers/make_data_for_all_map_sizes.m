a9d14801f9cf3ddbcdcc9e724f9ff2e288e69b76
Kate Harrison <harriska@eecs.berkeley.edu>
2013-06-25 16:48:26 -0700

    Updated the help message for make_data_for_all_map_sizes().

--- Helpers/make_data_for_all_map_sizes.m
+++ Helpers/make_data_for_all_map_sizes.m
@@ -1,4 +1,8 @@
 function [] = make_data_for_all_map_sizes(label)
+%   [] = make_data_for_all_map_sizes(label)
+%
+%   Make the data for all map sizes (as specified by
+%   get_simulation_value('valid_map_sizes')) for the given label.
 
 if ~isfield(label, 'map_size')
     error('Map size does not appear to matter for this label.');
