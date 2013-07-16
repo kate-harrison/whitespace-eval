7440f278c8f2f20038f3143710e0b1b2abc7666e
Kate Harrison <harriska@eecs.berkeley.edu>
2013-07-15 19:49:45 -0700

    Fixed a bug in save_plot() where it would not change the attributes (e.g. font name, size) of subplots, only those of the current axes.

--- Helpers/save_plot.m
+++ Helpers/save_plot.m
@@ -14,44 +14,58 @@ format = lower(format);
 load 'plot_parameters.mat'
 
 %% Format the axes, title, legend, and colorbar
-% Tick marks
-set(gca, 'fontsize', font_size);
-set(gca, 'fontname', font_name)
+axes_list = findall(gcf,'type','axes');
+for a = 1:length(axes_list)
+    ax = axes_list(a)
 
-% X-axis label
-xlhand = get(gca,'xlabel');
-set(xlhand, 'fontsize',font_size);
-set(xlhand, 'fontname', font_name)
+    % Format the axis
+    set(ax, 'fontsize', font_size);
+    set(ax, 'fontname', font_name);
+    
+    % X-axis label
+    xlhand = get(ax,'xlabel')
+    set(xlhand, 'fontsize',font_size);
+    set(xlhand, 'fontname', font_name)
+    
+    % Y-axis label
+    ylhand = get(ax,'ylabel');
+    set(ylhand, 'fontsize',font_size);
+    set(ylhand, 'fontname', font_name)
 
-% Y-axis label
-ylhand = get(gca,'ylabel');
-set(ylhand, 'fontsize',font_size);
-set(ylhand, 'fontname', font_name)
+    % Format the title
+    try
+        thand = get(ax, 'title');
+        set(thand, 'fontname', font_name);
+        set(thand, 'fontsize', font_size);
+    catch % do nothing
+    end
+    
+    % Format the legend
+    try
+        lhand = legend(ax);
+        set(lhand, 'fontsize', font_size);
+        set(lhand, 'fontname', font_name);
+    catch % do nothing
+    end
+    
+    % Format the colorbar
+    try
+        chand = get(ax, 'colorbar');
+        set(chand, 'fontname', font_name);
+    catch % do nothing
+    end
+end
 
-% Legend
-lhand = legend(gca);
-set(lhand, 'fontsize', font_size);
-set(lhand, 'fontname', font_name)
 
-% Legend label
+% Format the legend label if one exists
 try
     hl = findobj(gcf,'Tag','legend');
     hl_t = get(hl,'Title');
     set(hl_t, 'fontsize', font_size);
-catch error
+catch
 end
 
 
-% Title
-thand = get(gca, 'title');
-set(thand, 'fontname', font_name);
-set(thand, 'fontsize', font_size);
-
-% Colorbar
-try
-    chand = get(gca, 'colorbar');
-    set(chand, 'fontname', font_name);
-end
 
 if (nargin >= 3 && varargin{1})
     set(gcf,'PaperPositionMode','auto');
